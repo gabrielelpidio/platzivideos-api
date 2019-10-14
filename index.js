fd9e4997b2ea57d50c12d8dc579d98ed23c317e0
Gabriel De Andrade <gabrieldeandradeleal@gmail.com>
2019-10-14 19:15:47 -0400

    Implementando Boom

--- index.js
+++ index.js
@@ -5,14 +5,25 @@ const app = express();
 const { config } = require('./config/index');
 const moviesApi = require('./routes/movies');
 
-const { logErrors, errorHandler } = require('./utils/middleware/errorHandlers');
+const {
+  logErrors,
+  errorHandler,
+  wrapErrors
+} = require('./utils/middleware/errorHandlers');
+const notFoundHandler = require('./utils/middleware/notFoundHandler');
 
 //body parser
 app.use(express.json());
 
+//routes
 moviesApi(app);
 
+//Catch 404
+app.use(notFoundHandler);
+
+//Errors middleware
 app.use(logErrors);
+app.use(wrapErrors);
 app.use(errorHandler);
 
 app.listen(config.port, () => {
--- utils/middleware/errorHandlers.js
+++ utils/middleware/errorHandlers.js
@@ -1,8 +1,9 @@
+const boom = require('@hapi/boom');
 const { config } = require('../../config');
 
 function withErrorStack(error, stack) {
   if (config.dev) {
-    return { error, stack };
+    return { ...error, stack };
   }
 
   return error;
@@ -13,13 +14,26 @@ function logErrors(err, req, res, next) {
   next(err);
 }
 
+function wrapErrors(err, req, res, next) {
+  if (!err.isBoom) {
+    next(boom.badImplementation(err));
+  }
+
+  next(err);
+}
+
 // eslint-disable-next-line
 function errorHandler(err, req, res, next) {
-  res.status(err.satus || 500);
-  res.json(withErrorStack(err.message, err.stack));
+  const {
+    output: { statusCode, payload }
+  } = err;
+
+  res.status(statusCode || 500);
+  res.json(withErrorStack(payload, err.stack));
 }
 
 module.exports = {
+  wrapErrors,
   logErrors,
   errorHandler
 };
